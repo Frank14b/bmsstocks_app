9379101f2dd6eaf46e2ea35f1cae9a6240b9c215
FrankFontcha <frank.fontcha@vaimanagement.co>
2023-01-25 07:43:04 +0400

    new update

--- renderer/src/components/pages/DashboardComponent.jsx
+++ renderer/src/components/pages/DashboardComponent.jsx
@@ -3,11 +3,43 @@ import Head from 'next/head';
 import { useAppContext } from '../../contexts/GlobalContext';
 import { useState } from 'react';
 import Utils from '../../utils/Utils';
+import ApiCalls from '../../utils/apiCalls';
+import { useEffect } from 'react';
 
 function DashboardComponent() {
 
   const mycontext = useAppContext();
+  const [loader, setLoader] = useState(false)
+  const [userBusiness, setUserBusiness] = useState([])
   const [userdata, setUserdata] = useState(JSON.parse(Utils.getLocalStorage("userData")))
+
+  useEffect(() => {
+    getUserBusiness()
+  }, [])
+
+  const getUserBusiness = (e) => {
+    e.preventDefault();
+
+    setLoader(true)
+
+    let datas = {
+      "user_id": JSON.parse(Utils.getLocalStorage("userData")).id,
+      "status": 1
+    }
+    var body = datas;
+
+    ApiCalls.user_request(body, "business/getall").then((response) => {
+      if (response.data.status) {
+        // e.reset()
+        setLoader(true)
+        setUserBusiness(response.data.data)
+      } else {
+        setLoader(false)
+      }
+    }).catch((err) => {
+      setLoader(false)
+    })
+  }
 
   return (
     <React.Fragment>
